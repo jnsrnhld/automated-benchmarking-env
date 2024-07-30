7f53882a9cf3d4ccd9715c6ca6c963f689c14d79
Jonas Arnhold <jonasarnhold@web.de>
2024-07-30 22:13:34 +0200

    Extend operator draft

--- operator/main.py
+++ operator/main.py
@@ -1,41 +1,39 @@
 import os
-import asyncio
-import threading
 import logging
-import kopf
+from time import sleep
 
-from kubernetes import client
-from template_util import TemplateUtil
-
-
-@kopf.on.create('SparkApplication')
-def pod_spec_handler(name, logger):
-    logger.info(f"Spark application {name} running")
-
-
-@kopf.on.update('SparkApplication')
-def pod_spec_handler(name, status, logger):
-    logger.info(f"Spark application {name} updated. Current Status is: {status}")
-
-
-def kopf_thread():
-    asyncio.run(kopf.operator())
-
+from kubernetes import config
+from kopf_operator import Operator
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    os.environ["KUBECONFIG"] = f"{os.getcwd()}/ansible/k8s_setup/kubespray/inventory/mycluster/artifacts/admin.conf"
 
-    operator = threading.Thread(target=kopf_thread)
-    operator.start()
+    ensureKubeconfigIsPresent()
+    Operator().start()
 
-    try:
-        # verify we can interact with spark operator
-        spark_app = TemplateUtil.template("ansible/charts/spark-pi.yaml", {'spark_version': '3.5.0'})
+    while True:
+        sleep(5)
 
-        extensions_api = client.ApiextensionsV1Api()
-        api_response = extensions_api.create_custom_resource_definition(spark_app)
-        logging.info(api_response)
+    # try:
+    # verify we can interact with spark operator
+    # spark_app = TemplateUtil.template(f"{os.getcwd()}/ansible/charts/spark-pi.yaml", {'spark_version': '3.5.0'})
+    #
+    # extensions_api = client.ApiextensionsV1Api()
+    # api_response = extensions_api.create_custom_resource_definition(spark_app)
+    # logging.info(api_response)
 
-    except KeyboardInterrupt:
-        print("Stopping the scheduler...")
+    # client = pymongo.MongoClient("localhost", 27017)
+
+    # except KeyboardInterrupt:
+    #     print("Stopping the scheduler...")
+
+
+def ensureKubeconfigIsPresent():
+    kubeconfig_path = os.getenv('KUBECONFIG')
+    if not kubeconfig_path:
+        raise ValueError("KUBECONFIG environment variable is not set")
+    config.load_kube_config()
+
+
+if __name__ == '__main__':
+    main()
