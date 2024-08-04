1c31ab077a06343fdbaa638ad62eaaa09937dd18
Jonas Arnhold <jonasarnhold@web.de>
2024-08-04 15:28:08 +0200

    Add connection to HDFS in operator

--- operator/main.py
+++ operator/main.py
@@ -1,8 +1,12 @@
-import os
 import logging
 import pymongo
 
 from time import sleep
+from hdfs import InsecureClient
+client = InsecureClient("http://localhost:9870")
+# also specifying the user=stackable does not help
+# client = InsecureClient("http://localhost:9870", user="stackable")
+print(client.status("/"))
 
 from kopf_operator import Operator
 from config import Config
@@ -17,11 +21,14 @@ def connect_to_mongodb():
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    Operator().start()  # operator works
-    connect_to_mongodb()  # mongodb connection works
+    # Operator().start()  # operator works
+    # connect_to_mongodb()  # mongodb connection works
 
-    while True:
-        sleep(5)
+    client = InsecureClient("http://hdfs-cluster-namenode-default-0.hdfs-cluster-namenode-default.default.svc.cluster.local:9870")
+    print(client.status("/"))
+
+    # while True:
+    #     sleep(5)
 
 
 if __name__ == '__main__':
