c13662da11e33de7dd82605b1631ea68f69c3ad6
Jonas Arnhold <jonasarnhold@web.de>
2024-08-08 13:59:17 +0200

    Extend HDFS setup

--- operator/main.py
+++ operator/main.py
@@ -3,10 +3,6 @@ import pymongo
 
 from time import sleep
 from hdfs import InsecureClient
-client = InsecureClient("http://localhost:9870")
-# also specifying the user=stackable does not help
-# client = InsecureClient("http://localhost:9870", user="stackable")
-print(client.status("/"))
 
 from kopf_operator import Operator
 from config import Config
@@ -15,7 +11,7 @@ from config import Config
 def connect_to_mongodb():
     client = pymongo.MongoClient(Config.MONGODB_CONNECTION_STRING)
     db = client.get_database(Config.MONGODB_DATABASE)
-    print(f"Connected! Collections: {db.list_collection_names()}")
+    print(f"Connected to MongoDB! Listing collections: {db.list_collection_names()}")
 
 
 def main():
@@ -24,11 +20,12 @@ def main():
     # Operator().start()  # operator works
     # connect_to_mongodb()  # mongodb connection works
 
-    client = InsecureClient("http://hdfs-cluster-namenode-default-0.hdfs-cluster-namenode-default.default.svc.cluster.local:9870")
+    # HDFS works
+    client = InsecureClient("http://34.32.17.185:32489")
     print(client.status("/"))
 
-    # while True:
-    #     sleep(5)
+    while True:
+        sleep(5)
 
 
 if __name__ == '__main__':
